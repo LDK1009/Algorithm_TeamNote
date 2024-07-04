3d4773d6bdb3d0163da7825e66682badc6429b19
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-07-05 07:58:13 +0900

    문제풀이

---
+++ 백준 단계별 문제/05_문자열/03_9086.py
@@ -0,0 +1,6 @@
+num = int(input()) # 테스트케이스 개수
+
+for i in range(num):
+    s = input()
+    print(s[0], end='')
+    print(s[-1])
---
+++ 백준 단계별 문제/05_문자열/05_11720.py
@@ -0,0 +1,4 @@
+n = int(input()) # 숫자의 개수
+numbers = list(map(int, input()))
+
+print(sum(numbers))    
